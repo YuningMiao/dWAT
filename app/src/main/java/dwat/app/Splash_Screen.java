1b5c9bb96765fa9cef0eb9d5db34e2f29b431648
Allison Meagher <almeagher1@gmail.com>
2016-03-30 16:46:58 -0500

    splashscreen, camera, basic history, suggestion pages made. swipe activity used. working? location needs work: history storage/retrieval

--- app/src/main/java/dwat/app/Splash_Screen.java
+++ app/src/main/java/dwat/app/Splash_Screen.java
@@ -1,14 +1,12 @@
 package dwat.app;
 
-//import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 
-
-public class Splash_Screen extends Activity {
+public class Splash_Screen extends Activity{
 
     private static int SPLASH_TIME = 3000;
 
@@ -18,12 +16,9 @@ public class Splash_Screen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-//        ImageView img = (ImageView) findViewById(R.id.splash_logo);
-//        img.setImageResource(R.drawable.dw_logo_1);
-
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void run(){
+            public void run() {
                 Intent intent = new Intent(Splash_Screen.this, Suggestion_Screen.class);
                 startActivity(intent);
                 finish();
@@ -32,7 +27,4 @@ public class Splash_Screen extends Activity {
         }, SPLASH_TIME);
     }
 
-    @Override
-    public void onBackPressed() {
-    }
 }
--- app/src/main/java/dwat/app/Suggestion_Screen.java
+++ app/src/main/java/dwat/app/Suggestion_Screen.java
@@ -1,15 +1,14 @@
 package dwat.app;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -19,28 +18,32 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
+
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
-public class Suggestion_Screen extends Activity {
+public class Suggestion_Screen extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
     private static final String TODO = "";
     String curDate;
     String curLoc;
     ListView sl1, sl2;
 	RelativeLayout screen;
     String[] histValues = new String[]{"Food Item 1", "Food Item 2", "Food Item 3", "Food Item 4", "Food Item 5", "Food Item 6"};
-    String[] locValues = new String[]{"Food based on Loc 1", "Food based on Loc 2", "Food based on Loc 3", "Food based on Loc 4", "Food based on Loc 5"};
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+	ArrayList<String> locValues = new ArrayList<String>(Arrays.asList("Food based on loc 1", "Food based on loc 2", "Food based on loc 3", "Food based on loc 4", "Food based on loc 5"));
+	ArrayAdapter<String> adapter, adapter2;
+	private GoogleApiClient mGoogleApiClient;
 
     private String getCurDate() {
         Calendar c = Calendar.getInstance();
@@ -51,21 +54,10 @@ public class Suggestion_Screen extends Activity {
     }
 
     private String getCurLocation() {
-//        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        CurLocationListner locListner = new CurLocationListner();
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return TODO;
-//        }
-//        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListner);
-//        return curLoc;
-		return null;
+		if(curLoc == null || curLoc == "")
+			return null;
+		else
+			return curLoc;
 	}
 
 	@Override
@@ -73,6 +65,17 @@ public class Suggestion_Screen extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_suggest2);
+
+		mGoogleApiClient = new GoogleApiClient
+				.Builder( this )
+				.enableAutoManage( this, 0, this )
+				.addApi( Places.GEO_DATA_API )
+				.addApi( Places.PLACE_DETECTION_API )
+				.addConnectionCallbacks( this )
+				.addOnConnectionFailedListener( this )
+				.build();
+
+		guessCurrentPlace();
 
 		screen = (RelativeLayout) findViewById(R.id.suggestScreen);
 		screen.setOnTouchListener(new OnSwipeTouchListener(Suggestion_Screen.this){
@@ -86,42 +89,39 @@ public class Suggestion_Screen extends Activity {
 			}
 		});
 
-
 		//final String curDate = new SimpleDateFormat("MM-dd-yyyy").format(new Date());
 
 		sl1 = (ListView) findViewById(R.id.suggestList1);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, histValues);
+		adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, histValues);
 
 		sl1.setAdapter(adapter);
 		sl1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				History newMeal;
-				if(getCurLocation() == null) {
+				if (getCurLocation() == null) {
 					newMeal = new History(parent.getAdapter().getItem(position).toString(), getCurDate());
-				}
-				else
+				} else
 					newMeal = new History(parent.getAdapter().getItem(position).toString(), getCurDate(), getCurLocation());
 				Toast.makeText(getApplicationContext(), newMeal.getHist(), Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(Suggestion_Screen.this, History_Screen.class);
+				Intent intent = new Intent(Suggestion_Screen.this, History_Screen.class);
 				intent.putExtra("meal", (Serializable) newMeal);
 				startActivity(intent);
 			}
 		});
 
 		sl2 = (ListView) findViewById(R.id.suggestList2);
-		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, locValues);
+		adapter2 = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, locValues);
 
 		sl2.setAdapter(adapter2);
 		sl2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				History newMeal;
-				if(getCurLocation() == null) {
+				if (getCurLocation() == null) {
 					newMeal = new History(parent.getAdapter().getItem(position).toString(), getCurDate());
-				}
-				else
+				} else
 					newMeal = new History(parent.getAdapter().getItem(position).toString(), getCurDate(), getCurLocation());
 				Toast.makeText(getApplicationContext(), newMeal.getHist(), Toast.LENGTH_SHORT).show();
 
@@ -131,60 +131,117 @@ public class Suggestion_Screen extends Activity {
 			}
 		});
 
-		ImageButton button = (ImageButton) findViewById(R.id.cameraButton);
-		button.setOnClickListener(new View.OnClickListener() {
+		ImageButton cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+		cameraButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), Camera_Main.class);
-				startActivityForResult(intent, 0);
+			Intent intent = new Intent(v.getContext(), Camera_Main.class);
+			startActivityForResult(intent, 0);
 			}
 		});
-		// ATTENTION: This was auto-generated to implement the App Indexing API.
-		// See https://g.co/AppIndexing/AndroidStudio for more information.
-		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+		ImageButton refreshButton = (ImageButton) findViewById(R.id.refreshButton);
+		refreshButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			if (mGoogleApiClient.isConnected()) {
+				if (ContextCompat.checkSelfPermission(Suggestion_Screen.this,
+						Manifest.permission.ACCESS_FINE_LOCATION)
+						!= PackageManager.PERMISSION_GRANTED) {
+					ActivityCompat.requestPermissions(Suggestion_Screen.this,
+							new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+							1000);
+				} else {
+					guessCurrentPlace();
+				}
+			}
+			}
+		});
 	}
 
 	@Override
-	public void onStart() {
+	protected void onStart(){
 		super.onStart();
-
-		// ATTENTION: This was auto-generated to implement the App Indexing API.
-		// See https://g.co/AppIndexing/AndroidStudio for more information.
-		client.connect();
-		Action viewAction = Action.newAction(
-				Action.TYPE_VIEW, // TODO: choose an action type.
-				"Suggestion_Screen Page", // TODO: Define a title for the content shown.
-				// TODO: If you have web page content that matches this app activity's content,
-				// make sure this auto-generated web page URL is correct.
-				// Otherwise, set the URL to null.
-				Uri.parse("http://host/path"),
-				// TODO: Make sure this auto-generated app deep link URI is correct.
-				Uri.parse("android-app://dwat.app/http/host/path")
-		);
-		AppIndex.AppIndexApi.start(client, viewAction);
+		if (mGoogleApiClient != null)
+			mGoogleApiClient.connect();
 	}
 
 	@Override
-	public void onStop() {
+	protected void onStop() {
+		if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
 		super.onStop();
-
-		// ATTENTION: This was auto-generated to implement the App Indexing API.
-		// See https://g.co/AppIndexing/AndroidStudio for more information.
-		Action viewAction = Action.newAction(
-				Action.TYPE_VIEW, // TODO: choose an action type.
-				"Suggestion_Screen Page", // TODO: Define a title for the content shown.
-				// TODO: If you have web page content that matches this app activity's content,
-				// make sure this auto-generated web page URL is correct.
-				// Otherwise, set the URL to null.
-				Uri.parse("http://host/path"),
-				// TODO: Make sure this auto-generated app deep link URI is correct.
-				Uri.parse("android-app://dwat.app/http/host/path")
-		);
-		AppIndex.AppIndexApi.end(client, viewAction);
-		client.disconnect();
 	}
+
+	private void guessCurrentPlace() {
+		PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace( mGoogleApiClient, null );
+		result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+			@Override
+			public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+				if (!likelyPlaces.getStatus().isSuccess()) {
+					Log.e("TAG", "Place query didn't complete. Error: " + likelyPlaces.getStatus().toString());
+					likelyPlaces.release();
+					return;
+				}
+				try {
+					PlaceLikelihood placeLikelihood = likelyPlaces.get(0);
+					String content = "";
+					if (placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty(placeLikelihood.getPlace().getName()))
+						content = placeLikelihood.getPlace().getName() + "";
+					if (placeLikelihood != null)
+						content += " " + (int) (placeLikelihood.getLikelihood() * 100) + "%";
+
+					locValues.add("Meal based on " + content);
+					adapter2.notifyDataSetChanged();
+					curLoc = content;
+					Toast.makeText(getApplicationContext(), content, Toast.LENGTH_LONG).show();
+
+				} catch (IllegalStateException e) {
+					Log.w("TAG", "Fail to get place or its coordinates");
+				}
+
+				likelyPlaces.release();
+			}
+		});
+	}
+
 
 	@Override
 	public void onBackPressed() {
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		Log.e("TAG", "Google Places API connection failed with error code: "
+				+ connectionResult.getErrorCode());
+
+		Toast.makeText(this,
+				"Google Places API connection failed with error code:" +
+						connectionResult.getErrorCode(),
+				Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+										   String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case 1000:
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					guessCurrentPlace();
+				}
+				break;
+		}
 	}
 }
